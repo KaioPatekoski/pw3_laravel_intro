1fa12af87b0d19d883ca4b1a615070c7301bdaf1
Kaio Patekoski <kaiopatekoski33@gmail.com>
2026-08-07 16:35:52 -0300

    wip: layout padrao

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Inicio')
+
+@section('content')
+    <section>
+        <h2>Bem-Vindo ao projeto</h2>
+        <p>Esta tela reutiliza o layout padrao com header e footer.</p>
+    </section>
+@endsection
