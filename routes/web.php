11e795acab451c27aeddcdcb8945277afa7cf38b
Kaio Patekoski <kaiopatekoski33@gmail.com>
2026-08-12 16:42:54 -0300

    wip: landing page

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::view('/landing', 'landing');
+
 Route::get('/livros', [LivroController::class, 'index']);
 
 Route::post('/livros', [LivroController::class, 'store']);
