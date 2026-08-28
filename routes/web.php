73136a6caa0776127664ca9daa46c9f24f9a477b
Kaio Patekoski <kaiopatekoski33@gmail.com>
2026-08-28 17:27:54 -0300

    feat: introdução eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivroController;
 
@@ -9,6 +10,16 @@ Route::get('/', function () {
 
 Route::view('/landing', 'landing');
 Route::view('/admin', 'admin.dashboard');
+
+Route::get('/teste-orm', function (){
+    User::create([
+        'name' => 'Ana Clara Santos',
+        'email' => 'ana.santos@escola.sp.gov.br',
+        'password' => '12345678',
+    ]);
+
+    return User::all();
+});
 
 Route::get('/livros', [LivroController::class, 'index']);
 
